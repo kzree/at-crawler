1188825301dc320479b06b7c4792111fb27d9a35
Erkki Sula <43129445+kzree@users.noreply.github.com>
2021-09-10 20:12:52 +0300

    Documentation improvements

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ class Config(NamedTuple):
     max: int
 
 def init_parser():
+    """
+    Initializes the parser and the arguments used in the application.
+    """
     parser = argparse.ArgumentParser(description="Crawler for Arvutitark website for checking prices")
     parser.add_argument("item_name", type=str, help="Name of the item the program will look for")
     parser.add_argument("-min", type=int, help="Minimum value of item (default: 0)")
@@ -14,6 +17,12 @@ def init_parser():
     return parser
 
 def check_args_for_errors(parser, parsed_args):
+    """
+    Checks for false input from the user
+
+    @param parser: Active parser object
+    @param parsed_args: User input from the parser
+    """
     if (parsed_args.min is not None):
         if (parsed_args.min < 0):
             parser.error("Argument min cannot be negative")
@@ -24,6 +33,11 @@ def check_args_for_errors(parser, parsed_args):
             parser.error("Argument max cannot be negative")
 
 def get_application_arguments():
+    """
+    Gets the application arguments from user input, checks them and returns a Config object.
+
+    @returns config: A NamedTuple containing input from the user. Default values are None or 0.
+    """
     parser = init_parser()
     parsed_args = parser.parse_args()
     check_args_for_errors(parser, parsed_args)
--- crawler.py
+++ crawler.py
@@ -5,6 +5,15 @@ from bs4 import BeautifulSoup
 BASE_URL = "https://arvutitark.ee/est/Otsing?q="
 
 def is_price_in_bounds(price, min, max):
+    """
+    Checks if the item price is in given min and max bounds
+
+    @param price: Price that is checked
+    @param min: Given minimum price
+    @param max: Given maximum price, can be None type
+
+    @returns boolean
+    """
     if (price < min):
         return False
     if (max is not None and price > max):
@@ -15,6 +24,12 @@ def get_price(prices, min, max):
     """
     Checks both of the prices that are contained in the price container.
     The second price is always 0.00€ unless there is a sale.
+
+    @param prices: Prices in the item card, first price is regular price, second price is on sale price 
+    @param min: Given minimum price
+    @param max: Given maximum price, can be None type
+
+    @returns price: If the value is in given bounds returns price, else returns None
     """
     price = ""
     for pr in prices:
@@ -27,6 +42,11 @@ def get_item(card, conf):
     """
     Gets card element and checks if it has h2 in it, only product cards
     have a h2 element in it.
+
+    @param card: The item card element
+    @param conf: User input params config
+
+    @returns tuple: returns tuple with the item name and price
     """
     title = card.find("h2")
     price = ""
@@ -38,12 +58,24 @@ def get_item(card, conf):
     return (title, price)
 
 def get_last_page(pagination):
-    """Gets the last page number from the pagination element"""
+    """
+    Gets the last page number from the pagination element
+
+    @param pagination: Pagination element
+
+    @returns int: Last page number
+    """
     pagination_items = pagination.find_all("li")
     return int(pagination_items[len(pagination_items) - 2].text)
 
 def get_page_data(item_list, page, conf):
-    """Gets all the cards on the page and extracts the data from the cards"""
+    """
+    Gets all the cards on the page and extracts the data from the cards
+
+    @param item_list: List where parsed items are added to
+    @param page: Parsed page data
+    @param conf: User input params config
+    """
     cards = page.find_all("div", class_="card")
 
     for card in cards:
@@ -52,10 +84,22 @@ def get_page_data(item_list, page, conf):
             item_list.append(item)
 
 def get_parsed_page(url):
+    """
+    Gets the parsed data from given url
+
+    @param url: Url to be parsed
+
+    @returns page: Parsed page data
+    """
     page = requests.get(url)
     return BeautifulSoup(page.content, "html.parser")
 
 def print_list(items):
+    """
+    Prints out the parsed items
+
+    @param items: Item list containing all the parsed items
+    """
     for item in items:
         item_data = "{} - {}".format(item[1], item[0])
         print((item_data[:97] + '...') if len(item_data) > 100 else item_data)
